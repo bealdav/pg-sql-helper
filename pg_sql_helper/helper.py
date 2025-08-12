d963783e5d61ba7507da76803a453086b248a329
David Beal <david.beal@akretion.com>
2025-08-12 23:11:17 +0200

    implements date conversion

--- pg_sql_helper/helper.py
+++ pg_sql_helper/helper.py
@@ -12,7 +12,7 @@ class PgSqlHelper:
     # dsn
     conn: str = False
     # convert datetime to date
-    datetime_to_date: bool = False
+    datetime_to_date: bool = True
     # convert JSON to string according to local string
     lang: str = None
     # rename columns as friendly names
@@ -25,7 +25,7 @@ class PgSqlHelper:
         password: str,
         host: str = "localhost",
         port: int = 5432,
-        lang: str = None,
+        lang: str = "en_US",
     ) -> None:
         self.conn = f"postgres://{user}:{password}@{host}:{port}/{db}"
 
@@ -42,7 +42,7 @@ class PgSqlHelper:
             df = self._rename_columns(df)
         if self.lang:
             df = self._select_key_according_to_lang(df)
-        return df
+        return df.get_column("column").to_list()
 
     def set_columns_to_rename(self, columns: dict[str, str]):
         self.columns = columns
@@ -54,8 +54,19 @@ class PgSqlHelper:
         return df
 
     def _convert_to_date(self, df):
-        renames = {x: f"{x}::date" for x in df.filter(pl.col("type") == "timestamp")}
-        return df.rename(renames)
+        renames = {
+            x: f"{x}::date"
+            for x in df.filter(pl.col("type") == "timestamp")
+            .get_column("column")
+            .to_list()
+        }
+
+        def replace_values(value):
+            return renames.get(value, value)
+
+        if renames:
+            df = df.with_columns(pl.col("column").map_elements(replace_values))
+        return df
 
     def _get_columns_query(self, table):
         """udt_name values are:
@@ -65,5 +76,5 @@ class PgSqlHelper:
         SELECT column_name AS column, udt_name AS type
         FROM information_schema.columns
         WHERE table_schema = 'public'
-          AND table_name = {table}
+          AND table_name = '{table}'
         """
